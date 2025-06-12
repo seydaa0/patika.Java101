0c454f8826c632b2fb7e20cb89a056fc5542e7a9
seydaa0 <seydakocabas440@gmail.com>
2025-06-12 14:09:16 +0300

    patika.Java101

---
+++ Siralama/src/Main.java
@@ -0,0 +1,41 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1. sayı : ");
+        a = input.nextInt();
+
+        System.out.print("2. sayı : ");
+        b = input.nextInt();
+
+        System.out.print("3. sayı : ");
+        c = input.nextInt();
+
+        if(a<b && a<c){
+            if(b<c){
+                System.out.print(a + "<" + b + "<" + c);
+            }else{
+                System.out.print(a + "<" + c + "<" + b);
+            }
+        }else if(b<a && b<c){
+            if(a<c){
+                System.out.print(b + "<" + a + "<" + c);
+            }else{
+                System.out.print(b + "<" + c + "<" + a);
+            }
+        }else{
+            if(a<b){
+             System.out.print(c + "<" + a + "<" + b);
+            }else{
+             System.out.print(c + "<" + b + "<" + a);
+            }
+        }
+
+
+
+    }
+}
