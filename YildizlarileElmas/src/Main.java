0b51c10e3ebd20e78f6ab5ae7bac80faeb28947d
seydaa0 <seydakocabas440@gmail.com>
2025-06-15 14:56:25 +0300

    YildizlarileElmas

---
+++ YildizlarileElmas/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n -1 ; i >= 1; i--) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
